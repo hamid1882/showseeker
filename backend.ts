8adefb4af06b30b160bd0c53a6aefa2228f60050
hamid1882 <hussainhafeez1100@gmail.com>
2023-09-12 13:06:59 +0530

    Feat: Integrated Wrangler and deployed api/users on the Cloudflare worker"

---
+++ backend.ts
@@ -0,0 +1,19 @@
+
+
+addEventListener('fetch', (event) => {
+    // @ts-ignore
+    event.respondWith(handleRequest(event.request));
+  });
+  
+  async function handleRequest(request: Request): Promise<Response> {
+    const data = {
+      message: 'Hello, World!',
+      timestamp: new Date().toISOString(),
+    };
+  
+    const jsonResponse = JSON.stringify(data);
+  
+    return new Response(jsonResponse, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
